4d7cc08016fe44dbc22ab019ac48b69f1a719515
Vladimir <wizart88@gmail.com>
2022-04-04 23:48:28 +0400

    create adaptive index.html

--- app/js/main.js
+++ app/js/main.js
@@ -11,6 +11,9 @@ $(function () {
 		$('.menu').toggleClass('menu--open')
 	})
 
+	if($(window).width() < 651){
+		$('.works-path__item--measuring').appendTo($('.works-path__items-box'))
+	}
 
 	$('.top__slider').slick({
 		dots: true,
